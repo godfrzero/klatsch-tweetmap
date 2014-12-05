2aa0b773308f445c200fd18795cc7e7ef3031768
Vivek Thuravupala <quiksilvrgreen@gmail.com>
2014-12-05 16:38:37 +0530

    Connected to PG db, created happyPeople and sadPeople tables

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
   IMPORTING STUFF
 ******************************************************************************/
 var express = require('express');
+var pg = require('pg');
 
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -25,6 +26,30 @@ app.set('view engine', 'jade');
 
 // Get the port
 app.set('port', (process.env.PORT || 5000));
+
+/******************************************************************************
+  DATABASE CONNECTION
+******************************************************************************/
+var client = new pg.Client({
+    user: "twhaizfycqcdcw",
+    password: "io1A6aoh7FVfk4OB58eSwm_q4P",
+    database: "dca3vt6esl5tji",
+    port: 5432,
+    host: "ec2-54-83-204-244.compute-1.amazonaws.com",
+    ssl: true
+});
+
+client.connect(function(err, client) {
+  if(err) {
+    return console.log('Error connecting to DB', err);
+  }
+
+  var query = client.query('SELECT * FROM happyPeople');
+
+  query.on('row', function(row) {
+    console.log(JSON.stringify(row));
+  });
+});
 
 /******************************************************************************
   ROUTES
